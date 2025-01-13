9771f5ce4273e99003d0d263d3324c575f001504
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2025-01-13 22:46:43 +0700

    update UI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
+const logo = document.querySelector(".logo");
 const burgerBtn = document.querySelector(".burger_btn");
 const navbarList = document.querySelector(".navbar_list");
 
 const handleBurgerBtn = () => {
   navbarList.classList.toggle("w-full");
-  navbarList.classList.toggle("w-0");
 
   burgerBtn.src.includes("icon-hamburger")
     ? (burgerBtn.src = "assets/images/icon-close.svg")
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
           md: "1080px",
         },
       },
+      backgroundImage: {
+        "hero-desktop": "url('../images/desktop/image-hero.jpg')",
+        "hero-mobile": "url('../images/mobile/image-hero.jpg')",
+      },
       fontFamily: {
         primary: ["Alata", "sans-serif"],
         josefin: ["Josefin Sans", "sans-serif"],
+      },
+
+      colors: {
+        "dark-gray": "hsl(0, 0%, 55%)",
+        "darker-gray": "hsl(0, 0%, 41%)",
       },
     },
   },
