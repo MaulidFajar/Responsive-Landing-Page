d3b081021308dd8f4ee9ec7d29797f78fda1d36e
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2025-01-13 23:06:30 +0700

    more update UI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 const logo = document.querySelector(".logo");
 const burgerBtn = document.querySelector(".burger_btn");
-const navbarList = document.querySelector(".navbar_list");
+const navbarList = document.querySelector(".nav_list");
 
-const handleBurgerBtn = () => {
+burgerBtn.addEventListener("click", handleBurgerBtn);
+
+function handleBurgerBtn() {
   navbarList.classList.toggle("w-full");
+  logo.classList.toggle("fixed");
+  burgerBtn.classList.toggle("fixed");
 
   burgerBtn.src.includes("icon-hamburger")
     ? (burgerBtn.src = "assets/images/icon-close.svg")
     : (burgerBtn.src = "assets/images/icon-hamburger.svg");
-};
+}
